138de0e108ba6d0833fb5acdcd161a8864c8d444
GiacomoArena <giacomoarena28@gmail.com>
2023-04-27 15:35:39 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+
+<template>
+  <H1>HEADER</H1>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Info.vue
@@ -0,0 +1,15 @@
+  <script>
+  export default {
+    name: 'Info'
+  }
+  </script>
+
+<template>
+  
+  <H1>INFO CONTACT</H1>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
